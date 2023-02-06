f9c748e7627951e293feed88c175949166c25080
Samuel-Fortune <samuelchibu04@gmail.com>
2023-02-06 19:09:00 +0100

    responsiveness

--- js/index.js
+++ js/index.js
@@ -89,7 +89,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         //Apply this style to my nav buttom list
         document.getElementById("nav-buttom-list-menu").classList.remove("nav-buttom-list-normal");
-        ducument.getElementById("nav-buttom-list-menu").classList.add("nav-buttom-list");
+        document.getElementById("nav-buttom-list-menu").classList.add("nav-buttom-list");
 
         //Apply this style to my slider
         document.getElementById("slider").classList.remove("slider-normal");
@@ -107,7 +107,7 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("trip-advisor-wrapper").classList.remove("trip-advisor-wrapper-normal");
 
         //Apply this style to my guest review section
-        document.getElementById("").classList.remove("guest-review-section-normal");
+        document.getElementById("guest-review-section").classList.remove("guest-review-section-normal");
 
         //Apply this style to my newsletter section
         document.getElementById("newsletter-section").classList.remove("newsletter-section-normal");
@@ -118,9 +118,73 @@ document.addEventListener("DOMContentLoaded", function () {
         //Apply thiis style to my footer section
         document.getElementById("footer-section").classList.remove("footer-section-normal");
 
+        //Apply this style to my nav main for pc screen
+        document.getElementById("nav-main").classList.add("nav-main-large");
+        document.getElementById("nav-main").classList.remove("nav-main");
+
+        //Apply this style to my site wrapper
+        let elements = document.getElementsByClassName("site-wrapper");
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.width = "90%";
+
+        };
+
+        //Apply this style to my slider on pc screen 
+        document.getElementById("slider").classList.add("slider-large");
+        document.getElementById("slider").classList.remove("slider");
+
+        //Apply this style to my package menu on pc screen 
+        document.getElementById("package-menu").classList.add("package-menu-large");
+        document.getElementById("package-menu").classList.remove("package-menu");
+
+        //Apply this style to my package offer on pc screen 
+        document.getElementById("package-offer").classList.add("package-offer-large");
+        document.getElementById("package-offer").classList.remove("package-offer");
+
+        //Apply this style to my summary details for pc screens
+        document.getElementById("package-card-details").classList.add("package-card-details-large");
+        document.getElementById("package-card-details").classList.remove("package-card-details");
+
+        //Apply this style to my trip advisor wrapper for pc screen
+        document.getElementById("trip-advisor-wrapper").classList.add("trip-advisor-wrapper-large");
+        document.getElementById("trip-advisor-wrapper").classList.remove("trip-advisor-wrapper");
+
+        //Apply this style to my guest-review-section for pc screen
+        document.getElementById("guest-review-section").classList.add("guest-review-section-large");
+        document.getElementById("guest-review-section").classList.remove("guest-review-section");
+
+        //Apply this style to my activities section
+        document.getElementById("activities-section").classList.add("activities-section-large");
+        document.getElementById("activities-section").classList.remove("activities-section");
+
+        //Apply this style to my footer for pc screen
+        document.getElementById("footer-section").classList.add("footer-section-large");
+        document.getElementById("footer-section").classList.remove("footer-section");
+
+
     } else {
         // Apply styles for large screens
-        console.log('not working');
+        // console.log('not working');
+        document.getElementById("nav-main").classList.remove("nav-main-large");
+        //
+        document.getElementById("slider").classList.remove("slider-large");
+        //
+        document.getElementById("package-menu").classList.remove("package-menu-large");
+        //
+        document.getElementById("package-offer").classList.remove("package-offer-large");
+        //
+        document.getElementById("package-card-details").classList.remove("package-card-details-large");
+        //
+        document.getElementById("trip-advisor-wrapper").classList.remove("trip-advisor-wrapper-large");
+        //
+        document.getElementById("guest-review-section").classList.remove("guest-review-section-large");
+        //
+        document.getElementById("activities-section").classList.remove("activities-section-large");
+        //
+        document.getElementById("footer-section").classList.remove("footer-section-large");
+
+
+
     }
 
 
